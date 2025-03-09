82aa88b5f5f8ccbc714fe01212ad78489def4264
Danny Chen <dannychen@Dannys-Air.lan1>
2025-03-09 19:40:29 -0400

    refactor: add crit to card info display

--- src/components/UnitDetails.jsx
+++ src/components/UnitDetails.jsx
@@ -2,17 +2,26 @@ import CardDetail from './CardDetail'
 
 const UnitDetails = ({ hoveredCard }) => {
   return (
-    <div className='grid grid-rows-3 grid-cols-6'>
-      <CardDetail className='col-span-2' header='Grade' info={hoveredCard.grade} />
-      <CardDetail className='col-span-2' header='Power' info={hoveredCard.power} />
-      <CardDetail className='col-span-2' header='Shield' info={hoveredCard.shield} />
-      <CardDetail className='col-span-3' header='Skill' info={hoveredCard.skill} textSize='text-xs' />
-      {hoveredCard.triggerType
-        ? <CardDetail className='col-span-3' header='Trigger' info={hoveredCard.triggerType} textSize='text-xs' />
-        : <CardDetail className='col-span-3' header='Icon' info={hoveredCard.icon ? hoveredCard.icon : 'None'} textSize='text-xs' />
-      }
-      <CardDetail className='col-span-3' header='Nation' info={hoveredCard.nation} textSize='text-xs' />
-      <CardDetail className='col-span-3' header='Race' info={hoveredCard.race} textSize='text-xs' />
+    <div className='grid grid-rows-3'>
+      <div className='grid grid-cols-3'>
+        <CardDetail header='Grade' info={hoveredCard.grade} />
+        <CardDetail header='Power' info={hoveredCard.power} />
+        <CardDetail header='Shield' info={hoveredCard.shield} />
+      </div>
+
+      <div className='grid grid-cols-5'>
+        <CardDetail className='col-span-2' header='Skill' info={hoveredCard.skill} textSize='text-xs' />
+        {hoveredCard.triggerType
+          ? <CardDetail className='col-span-2' header='Trigger' info={hoveredCard.triggerType} textSize='text-xs' />
+          : <CardDetail className='col-span-2' header='Icon' info={hoveredCard.icon ? hoveredCard.icon : 'None'} textSize='text-xs' />
+        }
+        <CardDetail header='Critical' info={hoveredCard.critical} textSize='text-xs' />
+      </div>
+
+      <div className='grid grid-cols-2'>
+        <CardDetail header='Nation' info={hoveredCard.nation} textSize='text-xs' />
+        <CardDetail header='Race' info={hoveredCard.race} textSize='text-xs' />
+      </div>
     </div>
   )
 }
